b017596e3ee4b7100268439b0de214d034d43523
wampirovka <github@paiskr.eu>
2026-09-17 00:35:38 +0200

    Use Coil 2 imports for Kotlin 2.0

--- app/src/main/java/cz/openradio/player/StationLogo.kt
+++ app/src/main/java/cz/openradio/player/StationLogo.kt
@@ -14,7 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
+import coil.compose.AsyncImage
 
 @Composable
 fun StationLogo(
