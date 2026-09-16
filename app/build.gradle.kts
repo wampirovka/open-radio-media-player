3224043878675ca61c542ac597e4adf85fe4d32c
wampirovka <github@paiskr.eu>
2026-09-16 22:39:40 +0200

    Align Java and Kotlin JVM targets to 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,15 @@ android {
         versionCode = 1
         versionName = "0.1.0"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    kotlinOptions {
+        jvmTarget = "21"
+    }
 }
 
 dependencies {
