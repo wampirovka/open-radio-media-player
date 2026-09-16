9fe631fe04180af83661a3a7ee3cdac80a155061
wampirovka <github@paiskr.eu>
2026-09-16 23:28:05 +0200

    Add Room database

---
+++ app/src/main/java/cz/openradio/player/AppDatabase.kt
@@ -0,0 +1,29 @@
+package cz.openradio.player
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [StationEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun stationDao(): StationDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "open_radio.db"
+                ).build().also { INSTANCE = it }
+            }
+    }
+}
