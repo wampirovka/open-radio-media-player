12eaeab0207344bc847a69fb359f4b9dd69266dd
wampirovka <github@paiskr.eu>
2026-09-16 23:48:48 +0200

    Add Compose extended icons dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3:1.3.1")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.media3:media3-exoplayer:1.5.1")
     implementation("androidx.media3:media3-session:1.5.1")
     implementation("androidx.media3:media3-common:1.5.1")
