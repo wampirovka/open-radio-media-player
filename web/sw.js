7b237f0e5a4618c0a6a929db2cbf8ee96b22a8a0
wampirovka <github@paiskr.eu>
2026-09-22 23:42:40 +0200

    Refresh web app cache after player fix

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='open-radio-v3';
+const CACHE_NAME='open-radio-v4';
 const APP_SHELL=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
