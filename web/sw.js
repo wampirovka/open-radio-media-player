79d355a5cca37cd1ccca6d74f483cea1a9e501ea
wampirovka <github@paiskr.eu>
2026-09-22 23:17:00 +0200

    Fix web cache so redesigned UI updates

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='open-radio-v1';
+const CACHE_NAME='open-radio-v2';
 const APP_SHELL=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
